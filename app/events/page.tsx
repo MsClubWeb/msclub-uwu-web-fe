0b48c2428350787ec0f9c820726bf0162d1aed01
imalkadilakshan99 <imalkadilakshan99@gmail.com>
2025-05-25 00:58:49 +0530

    Change the propose an event Button

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -384,7 +384,7 @@ export default function EventsPage() {
               any tech event, we'd love to hear from you!
             </p>
             <div className="flex flex-col sm:flex-row gap-4 justify-center">
-              <Button asChild size="lg" variant="fluent" className="depth-2 motion-scale">
+              <Button asChild size="lg" variant="outline" className="border-white text-blue-600 hover:bg-white/20 depth-1 motion-scale">
                 <Link href="/contact">
                   Propose an Event
                   <ChevronRight className="ml-2 h-5 w-5" />
